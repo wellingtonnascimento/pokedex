4b0675439ded0ca9e77ee32ab5f703359b60870e
Wellington Nascimento <wellingto_07@hotmail.com>
2021-10-15 16:26:18 -0300

    feat: background gradient

--- src/styles/global.js
+++ src/styles/global.js
@@ -15,7 +15,8 @@ const GlobalStyles = createGlobalStyle`
   body {
     font-family: Roboto, Oxygen,Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     color: #f3f3f3;
-    background: #ccc;
+    background: rgb(2,0,36);
+    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 1%, rgba(6,70,161,1) 24%, rgba(0,212,255,1) 74%);;
   }
   #root {
     max-width: 1080px;

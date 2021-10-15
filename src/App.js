d6b8b1c3f0783ed8ab58f4117ab346fe2e5c0a70
Wellington Nascimento <wellingto_07@hotmail.com>
2021-10-15 16:29:19 -0300

    Feat: criação de buscar por pokemon/id api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,65 @@
-function App() {
-  return <div></div>;
-}
+import { useState } from "react";
+
+import api from "./services/api";
+
+import pokeball from "./assets/pokeball.svg";
+import GlobalStyles from "./styles/global";
+import { Wrapper, Welcome, Form, Input, Button } from "./styles/styles";
+
+const App = () => {
+  const [pokemon, setPokemon] = useState("");
+  const [error, setError] = useState(null);
+  const [typedPokemon, setTypedPokemon] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = (event) => {
+    setTypedPokemon(event.target.value.toLowerCase());
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!typedPokemon) {
+      return;
+    }
+    setIsLoading(true);
+    try {
+      const response = await api.get(`/pokemon/${typedPokemon}`);
+      console.log(response.data);
+      setPokemon(response.data);
+      // setError(null);
+      // setIsLoading(false);
+    } catch (error) {
+      setError("Pokemon não encontrado!");
+      setIsLoading(false);
+      setPokemon(null);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Welcome>Pokémon Pokédex!</Welcome>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          value={typedPokemon}
+          placeholder="Nome do pokemon/id"
+          onChange={handleChange}
+          hasError={!!error}
+        />
+        <Button type="submit">
+          {isLoading ? (
+            <span>carregando...</span>
+          ) : (
+            <>
+              Buscar <img src={pokeball} alt="pokeball" />{" "}
+            </>
+          )}
+        </Button>
+      </Form>
+      {error && <span>{error}</span>}
+      {pokemon.name}
+      <GlobalStyles />
+    </Wrapper>
+  );
+};
 
 export default App;

33fa79db5d75211425b95e8078601e0d2b107c8a
Wellington Nascimento <wellingto_07@hotmail.com>
2021-10-15 16:31:28 -0300

    feat: styled form buscar pokemon

---
+++ src/styles/styles.js
@@ -0,0 +1,55 @@
+import styled, { css } from "styled-components";
+import { shade } from "polished";
+
+export const Wrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Welcome = styled.h1`
+  font-size: 4em;
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  display: flex;
+  height: 60px;
+`;
+
+export const Input = styled.input`
+  height: 50px;
+  font-size: 1.2em;
+  padding: 0 24px;
+  border-radius: 5px 0 0 5px;
+  color: #3a3a3a;
+  border: 2px solid #fff;
+  border-right: 0;
+  ${(props) =>
+    props.hasError &&
+    css`
+      border: 2px solid red;
+    `}
+`;
+
+export const Button = styled.button`
+  width: 210px;
+  background: #e3350c;
+  border-radius: 0 5px 5px 0;
+  border: 0;
+  color: #fff;
+  font-weight: bold;
+  transition: background-color 0.2s;
+  height: 50px;
+  img {
+    margin-left: 5px;
+    height: 20px;
+    width: 20px;
+    vertical-align: bottom;
+  }
+  &:hover {
+    background: ${shade(0.2, "#E3350C")};
+  }
+`;
